b4236aa63091832693494073c71bede3d8bf4311
tsuilouis <louis.tsui@mail.utoronto.ca>
2016-05-31 16:38:29 -0400

    Week 3 Fragments complete

--- Fragments/Lab3c_FragmentsLab/app/src/main/java/course/labs/fragmentslab/MainActivity.java
+++ Fragments/Lab3c_FragmentsLab/app/src/main/java/course/labs/fragmentslab/MainActivity.java
@@ -26,9 +26,11 @@ public class MainActivity extends Activity implements
 			mFriendsFragment = new FriendsFragment();
 
 			//TODO 1 - add the FriendsFragment to the fragment_container
-			
-			
-			
+			FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+
+			fragmentTransaction.add(R.id.fragment_container, mFriendsFragment);
+
+			fragmentTransaction.commit();
 
 		} else {
 
@@ -65,9 +67,13 @@ public class MainActivity extends Activity implements
 		if (!isInTwoPaneMode()) {
 
 			//TODO 2 - replace the fragment_container with the FeedFragment
-			
+			FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
 
-			
+			fragmentTransaction.replace(R.id.fragment_container, mFeedFragment);
+
+			fragmentTransaction.addToBackStack(null);
+
+			fragmentTransaction.commit();
 
 			// execute transaction now
 			getFragmentManager().executePendingTransactions();
